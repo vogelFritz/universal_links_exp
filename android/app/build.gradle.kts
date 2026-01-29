b33afb896859dc63abb420848c46d1fc11a9ad49
vogelFritz <franciscosantin2002@gmail.com>
2026-01-29 16:46:52 -0300

    update version and add https host

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
-        versionName = 1.0.1
+        versionCode = 4
+        versionName = "1.0.3"
     }
     signingConfigs {
         create("release") {
